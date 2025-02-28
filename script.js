b0a3a028a518bdcd65c6f433cbb21734a4914f4f
gauravzswal <gauravzswal123@gmail.com>
2025-02-28 14:05:36 +0530

    first push

---
+++ script.js
@@ -0,0 +1,54 @@
+const form = document.querySelector("form");
+const textarea = document.querySelector(".txtarea");
+const btn = document.querySelector(".btn");
+const color = document.querySelector("#color");
+const notesContainer = document.querySelector(".notesContainer");
+
+const createdNotes = [];
+const deletedNotes = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const note = {
+    text: textarea.value,
+    color: color.value,
+    timestamp: new Date().toLocaleString(),
+    position: createdNotes.length,
+  };
+
+  createdNotes.push(note);
+  textarea.value = "";
+  showNotes();
+});
+
+function showNotes() {
+  notesContainer.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+
+  createdNotes.forEach((note) => {
+    noteDiv = document.createElement("div");
+    noteDiv.classList.add("notes-box");
+
+    noteDiv.style.backgroundColor = note.color;
+
+    const text = document.createElement("p");
+    text.innerText = note.text;
+
+    const timestamp = document.createElement("span");
+    timestamp.classList.add("timestamp");
+    timestamp.innerText = note.timestamp;
+
+    const close = document.createElement("span");
+    close.classList.add("close");
+    close.innerHTML = "&times";
+    close.addEventListener("click", () => {
+      createdNotes.splice(note.position, 1);
+      showNotes();
+    });
+
+    noteDiv.append(text, close, timestamp);
+    fragment.append(noteDiv);
+  });
+
+  notesContainer.append(fragment);
+}
